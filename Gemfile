96eea3c2a2b85a4e6be0af6d847d4f37d09ea246
punsa <puneet.241994.agarwal@gmail.com>
2015-04-07 12:26:20 +0530

    advance changes on observing some issues of not giving notification of file modification when it is edited from gedit or some another editor. Now implementation is quite different

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in freespeed.gemspec
 gemspec
-rb-inotify
+
--- lib/freespeed.rb
+++ lib/freespeed.rb
@@ -1,6 +1,5 @@
 # Provides Ruby interface for Native system call libraries of linux.
 
-require "freespeed/version"
 require "rb-inotify"
 
 module Freespeed
@@ -31,23 +30,65 @@ module Freespeed
     # after FileUpdateChecker has been initialized.
     def initialize(files, dirs={}, &block)
       @files = files.freeze
+      @glob = compile_glob(dirs)
       @block = block
       @events_captured = []
       @modified = false
       @thr = nil
+      @watched = watched
+      @dir_hash = Hash.new
+      @files_hash = Hash.new
+      @last_mtime = Time.now
 
       @notifier = INotify::Notifier.new
+
+      @watched.each do |n_file|
+      		abs_path = File.expand_path(n_file)
+      		if @files_hash[abs_path].nil?
+      			@files_hash[abs_path] = true
+      		end
+      		dir_path = File.dirname(abs_path)
+      		abs_dir_path = File.expand_path(dir_path)
+      		if @dir_hash[abs_dir_path].nil?
+      			@dir_hash[abs_dir_path] = Array.new
+      			@dir_hash[abs_dir_path] << abs_path
+      		else
+      			@dir_hash[abs_dir_path] << abs_path
+      		end
+      end
+
+      # #printing dir_hash
+      # @dir_hash.each do |key,pair|
+      # 	puts "key - #{key} and pair - #{pair}"
+      # end
+
+      # #printing files_hash
+      # @files_hash.each do |key,pair|
+      # 	puts "key - #{key} and pair - #{pair}"
+      # end
+
+      #Adding watch to the file, it will execute the block if any file event is occured on the file.
+      @dir_hash.each do |key,pair| 
+      	@notifier.watch(key,:all_events) do |event|
+      		#puts "#{event.absolute_name} on #{event.flags}"
+      		@events_captured << "#{event.absolute_name}"
+      		check_status
+      	end
+      end
       
-      @files.each do |n_file|
-  	
-  	# Adding watch to the file, it will execute the block if any file event is occured on the file.
-        @notifier.watch("#{n_file.to_s}", :modify,:delete_self) do |event| 
-              @events_captured << "#{event.flags} event has occured on #{n_file.to_s} at #{Time.now}"
-              @modified = true
-              stop_file_notifier
-        end 
-      end 
       start_file_notifier
+    end
+
+    def check_status
+    
+    	@events_captured.each do |event|
+    		if @files_hash[event.to_s] && @last_mtime < File.mtime(event.to_s)
+    			@modified = true
+    			stop_file_notifier
+    			@events_captured = []
+    			break
+    		end
+    	end
     end
 
     # This method prints all the events captured by the notifier.
@@ -60,7 +101,7 @@ module Freespeed
     # This method starts taking events by starting the notifier in thread.
     def start_file_notifier
       @modified = false
-      stop_file_notifier if !@thr.nil?
+      stop_file_notifier
       @thr = Thread.new {@notifier.run}
     end
 
@@ -77,14 +118,44 @@ module Freespeed
     end
 
     # This method executes the block and return 'true' if file system is updated else false.
-    def execute_if_updated?
+    def execute_if_updated
+      #check_status
+      #puts updated?
       if updated?
         @block.call
+        @last_mtime = Time.now
+        @modified = false
         start_file_notifier
         true
       else
         false
       end
+    end
+
+    def compile_glob(hash)
+      hash.freeze # Freeze so changes aren't accidentally pushed
+      return if hash.empty?
+
+      globs = hash.map do |key, value|
+        "#{escape(key)}/**/*#{compile_ext(value)}"
+      end
+      "{#{globs.join(",")}}"
+    end
+
+    def escape(key)
+      key.gsub(',','\,')
+    end
+
+    def compile_ext(array)
+      array = Array(array)
+      return if array.empty?
+      ".{#{array.join(",")}}"
+    end
+
+    def watched
+        all = @files.select { |f| File.exist?(f) }
+        all.concat(Dir[@glob]) if @glob
+        all
     end
 
   end
